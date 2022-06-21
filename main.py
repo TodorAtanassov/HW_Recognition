33ca8b4ab37bd21edc65e57f37c431d2462944b5
TodorAtanassov <todoratanasov432@gmail.com>
2022-06-21 19:36:53 +0300

    ts

---
+++ main.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Dropout
+from keras.optimizers import SGD, Adam
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
