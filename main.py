2fb9845d47313e34a08dcebeb1f15f3dd770a5dc
TodorAtanassov <todoratanasov432@gmail.com>
2022-06-27 12:56:33 +0300

    ts

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import img as img
 import matplotlib.pyplot as plt
 import pandas as pd
 import tensorflow as tf
@@ -13,3 +14,32 @@ from sklearn.utils import shuffle
 
 data = pd.read_csv(r"E:\Python\Projects\samples\A_Z Handwritten Data.csv").astype('float32')
 print(data.head(10))
+
+X = data.drop('0',axis = 1)
+y = data['0']
+
+
+train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.2)
+train_x = np.reshape(train_x.values, (train_x.shape[0], 28, 28))
+test_x = np.reshape(test_x.values, (test_x.shape[0], 28, 28))
+print("Train data shape: ", train_x.shape)
+print("Test data shape: ", test_x.shape)
+
+
+word_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M',
+             13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y',
+             25: 'Z'}
+
+y_int = np.int0(y)
+count = np.zeros(26, dtype='int')
+for i in y_int:
+    count[i] += 1
+alphabets = []
+for i in word_dict.values():
+    alphabets.append(i)
+fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+ax.barh(alphabets, count)
+plt.xlabel("Number of elements ")
+plt.ylabel("Alphabets")
+plt.grid()
+plt.show()
